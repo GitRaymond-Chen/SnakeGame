b358c5561f1a35818512d60e64316fcb7f55f6f7
Riskraider <chenraymond187@gmail.com>
2023-12-07 20:24:04 -0500

    styling

---
+++ script.js
@@ -0,0 +1,13 @@
+// Define HTML elements
+const board = document.getElementById("game-board");
+
+// Define game variables
+let snake =
+  // Draw game map, snake, food
+  function draw() {
+    board.innerHTML = "";
+    drwaSnake();
+  };
+
+// Draw snake
+function drwaSnake() {}
